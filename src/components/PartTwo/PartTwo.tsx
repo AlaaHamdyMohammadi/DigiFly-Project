f998c7bf85a24e0e2a5260c68317036b1ddc941b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-04-26 03:27:36 +0200

    Add map

--- src/components/PartTwo/PartTwo.tsx
+++ src/components/PartTwo/PartTwo.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Content from './Content';
+import LeafletMap from './LeafletMap';
 
 const PartTwo = () => {
   return (
     <main className="h-[969px]">
       <Content />
+      <LeafletMap/>
     </main>
   );
 }
